b55d26973db4dfadfbb5e50f43c92b2ecca7f492
伊藤翼 <tsubasa8770@gmail.com>
2019-09-17 01:04:15 +0900

    チャート表示まで（9）

---
+++ resources/js/App.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <div class="container">
+            <div class="navbar">
+                <span>Dashboad</span>
+            </div>
+        </div>
+        <div class="content">
+            <div class="panel">
+                <div class="panel-title">Dashboad</div>
+            </div>
+            <div class="card-row">
+                <div class="card-item" v-for="card in cards">
+                    <div class="card-inner">
+                        <div class="card-title">Card Title</div>
+                        <div class="card-value">100</div>
+                        <div class="card-chart">
+                            <la-cartesian :width="275" :height="40" :data="values">
+                                <la-area animated prop="value"></la-area>
+                            </la-cartesian>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+import { Cartesian, Area } from 'laue'
+
+    export default {
+        components : {
+            LaCartesian: Cartesian,
+            LaArea : Area
+        },
+        data (){
+            return{
+                cards : [1,2,3,4,5,6],
+                values : [
+                    {value : 0},
+                    {value : 1},
+                    {value : 10},
+                ]
+            }
+        }
+    }
+</script>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
-require('./bootstrap');
+import Vue from 'vue'
+import App from './App.vue'
+
+const app = new Vue({
+    el: '#app',
+    components: {
+        App,
+    },
+    template: '<app></app>'
+})
